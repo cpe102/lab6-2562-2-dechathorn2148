8e3d53c099b87960c74bd82784749f29df70e3f8
lab314-16@labeng.local <lab314-16@labeng.local>
2020-01-17 16:26:39 +0700

    finished

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,13 +2,29 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if(x == 'A')
+    {
+    	cout << "Z";
+	}
+    else
+	{
+		if(x <= 'Z' and x > 'A')
+		{
+			x = (char) x-1;
+		    cout << x;
+		}
+		else
+		{
+			cout << "0";
+		}
+	}
+	   
 }
 
 int main(){
 	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
+	cout << before('F') << "\n";
+	cout << before('f') << "\n";
 	cout << before('P') << "\n";
 	cout << before('T') << "\n";
 	cout << before('Z') << "\n";
